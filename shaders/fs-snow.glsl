315d256533668efbff14719fe920078dd7db515f
Isaac Cohen <isaaclandoncohen@gmail.com>
2014-11-07 18:15:14 +0100

    addings

--- shaders/fs-snow.glsl
+++ shaders/fs-snow.glsl
@@ -18,7 +18,7 @@ void main(){
 
   vec4 nCol =  vec4( vNorm * .5 + .7 , 1. );
 
-  vec4 color = nCol * sem + nCol * pow(( 1.-abs(vFR)) , 10. );
+  vec4 color = nCol;// * sem + nCol * pow(( 1.-abs(vFR)) , 10. );
 
   if( vID + vFade*( 1. + vEdge ) > filled ){
 
@@ -29,7 +29,7 @@ void main(){
   }
 
 
-  gl_FragColor = ( 1. - pow( vFR , 10. ))* color;// + vEdge * vEdge * vEdge * (vec4( 1.1 ) - color);// vec4( 1. , 0. , 1. , 1. );// vec4( normalize( vNorm ) , 1. );
+  gl_FragColor = vec4(( 1. - pow( vFR , 10. )) * color.xyz , color.w );// + vEdge * vEdge * vEdge * (vec4( 1.1 ) - color);// vec4( 1. , 0. , 1. , 1. );// vec4( normalize( vNorm ) , 1. );
  // gl_FragColor = vEdge * vec4( 1., 0. , 0. , 1. ) + vec4( 0. , 0. , 1. , 0. );//* color;// vec4( 1. , 0. , 1. , 1. );// vec4( normalize( vNorm ) , 1. );
   //gl_FragColor =vec4( vec3(max( 0. ,  dot( vNorm , vec3( 1. , 0. , 0. ) ))) , 1. );//* color;// vec4( 1. , 0. , 1. , 1. );// vec4( normalize( vNorm ) , 1. );
  // gl_FragColor =vec4( vec3( vFade , 0. , 0. )+ vec3( 0. , 0. , 1. ) , 1. );//* color;// vec4( 1. , 0. , 1. , 1. );// vec4( normalize( vNorm ) , 1. );
